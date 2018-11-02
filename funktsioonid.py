0200108695f138dc1fa37717f5a1db702c2dbf81
rteessar <rteessar@gmail.com>
2018-11-02 15:32:10 +0200

    Sai kõvasti edititud maini ja funktsioone, esialgne versioon esimesest tundidega päevast valmis. Lisatud ka tulevikuplaanid.

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,5 +1,12 @@
 import easygui
-def day1(tunniplaan):
-    
+def day1(tunniplaan,skillid):
     for tund in tunniplaan:
-        
+        osalemine = easygui.ynbox("Kas tahad " + tund + " loengus " +" osaleda?","Esimene päev",("Jah","Ei"), image="vanemuine46.png")
+        skillid[tund] = 0
+        if osalemine == True:
+            easygui.msgbox("Käisid loengus, tubli!","kiitused")
+            if skillid[tund] < 100:
+                skillid[tund] += 1
+        else:
+            easygui.msgbox('Jätsid loengus käimata!', 'Väga halb!')
+    return skillid
